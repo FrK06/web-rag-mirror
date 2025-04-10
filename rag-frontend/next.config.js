54bc5a83bf07b94a409e552c8a58440d58c1a1c3
FrK06 <146533768+FrK06@users.noreply.github.com>
2025-04-10 09:29:32 +0100

    Update next.config.js

--- rag-frontend/next.config.js
+++ rag-frontend/next.config.js
@@ -12,7 +12,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'https://auth-service-58a543c5-5a59-41e7-bee8-b8fb581add47.railway.app',
+        destination: 'https://auth-service-58a543c5-5a59-41e7-bee8-b8fb581add47.railway.app/api/:path*',
       },
     ];
   },
